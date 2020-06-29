82512f6a46402174568547a58537a04fe147ad31
Alexander Dolidze <alexander.dolidze@gmail.com>
2020-06-29 22:31:47 +0000

    implemented /api/whoami

--- server.js
+++ server.js
@@ -19,9 +19,12 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('Accept-Language'),
+    software: req.get('User-Agent')
+  });
 });
 
 
